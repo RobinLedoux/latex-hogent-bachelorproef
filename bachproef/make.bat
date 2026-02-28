c574030ffd27ffc41b1693c94771472bdab7e43b
Robin Ledoux <robin.ledoux@student.hogent.be>
2026-02-28 16:29:37 +0100

    Deel literatuurstudie

--- bachproef/make.bat
+++ bachproef/make.bat
@@ -1,5 +1,5 @@
 @echo off
-rem Absolute paths
+
 set "ROOT=C:\Users\Robin\OneDrive - Hogeschool Gent\2025-2026\Bachelorproef\latex-hogent-bachelorproef"
 set "OD=%ROOT%\output"
 set "TEX=%ROOT%\bachproef\LedouxRobinBP.tex"
@@ -7,7 +7,10 @@ set "TEX=%ROOT%\bachproef\LedouxRobinBP.tex"
 rem Ensure output directory exists
 if not exist "%OD%" mkdir "%OD%"
 
-rem Compile LaTeX with Minted + Biber
+rem CRITICAL: must match -output-directory EXACTLY
+set "TEXMF_OUTPUT_DIRECTORY=%OD%"
+
+rem Compile
 xelatex -shell-escape -output-directory="%OD%" "%TEX%"
 biber --input-directory="%OD%" --output-directory="%OD%" LedouxRobinBP
 xelatex -shell-escape -output-directory="%OD%" "%TEX%"
